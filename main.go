04fa87fa4472efaa8c346ac71bc9cd13f42e56d6
Ian Bishop <ian@pace7.com>
2018-09-25 10:19:59 +1000

    add -dimension option

--- main.go
+++ main.go
@@ -7,20 +7,41 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/aws/external"
 	"github.com/aws/aws-sdk-go-v2/service/cloudwatch"
 )
 
+type dimensions []cloudwatch.Dimension
+
+func (i *dimensions) String() string {
+	return fmt.Sprintf("%v", *i)
+}
+
+func (i *dimensions) Set(value string) error {
+	bits := strings.SplitN(value, "=", 2)
+	if len(bits) == 2 {
+		d := cloudwatch.Dimension{}
+		d.Name = &bits[0]
+		d.Value = &bits[1]
+		*i = append(*i, d)
+	}
+	return nil
+}
+
 func main() {
+
+	var dims dimensions
 
 	region := flag.String("region", "ap-southeast-2", "")
 	namespace := flag.String("namespace", "CustomMetrics", "")
-	metricName := flag.String("metric-name", "", "")
+	metricName := flag.String("metric-name", "", "the name of the metric")
 	unit := flag.String("unit", "Count", "Cloudwatch metric unit")
 	value := flag.Float64("value", 0, "")
 	resolution := flag.Int64("resolution", 60, "storage resolution for metric in seconds (1 or 60)")
+	flag.Var(&dims, "dimension", "name=value pair that uniquely identifies metric. Multiple allowed. Defaults to instanceID")
 	flag.Parse()
 
 	if *metricName == "" {
@@ -29,7 +50,7 @@ func main() {
 		os.Exit(1)
 	}
 
-	if *resolution != 1 || *resolution != 60 {
+	if *resolution != 1 && *resolution != 60 {
 		fmt.Printf("resolution must be either 1 or 60\n\n")
 		flag.PrintDefaults()
 		os.Exit(1)
@@ -44,19 +65,20 @@ func main() {
 		Config: cfg,
 	}
 
-	id, err := GetInstanceID()
-	if err != nil {
-		log.Fatal(err)
+	if dims == nil {
+		id, err := GetInstanceID()
+		if err != nil {
+			log.Fatal(err)
+		}
+		dimensionKey := "InstanceId"
+		dims = []cloudwatch.Dimension{
+			cloudwatch.Dimension{
+				Name:  &dimensionKey,
+				Value: &id,
+			},
+		}
 	}
-
-	dimensionKey := "InstanceId"
-	dimensions := []cloudwatch.Dimension{
-		cloudwatch.Dimension{
-			Name:  &dimensionKey,
-			Value: &id,
-		},
-	}
-	metricDatum := constructMetricDatum(*metricName, *value, *resolution, cloudwatch.StandardUnit(*unit), dimensions)
+	metricDatum := constructMetricDatum(*metricName, *value, *resolution, cloudwatch.StandardUnit(*unit), dims)
 	cloudWatch.Publish(metricDatum, *namespace)
 }
 
